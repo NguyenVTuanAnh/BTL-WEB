f6ef8a6f52d71e49998c464be2cf335abd312db9
NguyenVTuanAnh <nguyentuananhaz9@gmail.com>
2025-02-18 21:14:36 +0700

    init

---
+++ src/test/java/food_store/example/foodstore/FoodstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package food_store.example.foodstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
